60e3dcb2dddaa8fa75b3c1e30e7c31c36e4fedef
nathanverrilli <nathan.verrilli@gmail.com>
2020-11-19 20:36:19 -0800

    logging change

--- misc.go
+++ misc.go
@@ -5,15 +5,15 @@ import (
 	"os"
 )
 
-// for those irritating functions that should be deferred,
+// for those functions that should be deferred,
 // yet return errors, this handles & reports the error
 // cannot assume Log is {yet, still} active, so do not log
 func DeferError(f func() error) {
 	err := f()
 	if nil != err {
-		_, _ = fmt.Fprintln(
-			os.Stderr,
-			"(may be harmless) error in deferred function: "+err.Error(),
-		)
+		_, _ = fmt.Fprintf(os.Stderr,
+			"%s%s\n",
+			"(may be harmless) error in deferred function: ",
+			err.Error())
 	}
 }
